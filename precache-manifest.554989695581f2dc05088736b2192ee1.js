70d5bc90d222f3f696888ab131226fefe82d6c8a
Joel Rudin <jarud55@gmail.com>
2020-05-04 22:38:37 -0400

    Updates

---
+++ precache-manifest.554989695581f2dc05088736b2192ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b5c47fe5e4d939dd2b2c7dbcec7773",
+    "url": "/react-heifetz/index.html"
+  },
+  {
+    "revision": "831ae9014477a7b4a48c",
+    "url": "/react-heifetz/static/css/main.a8f57e2b.chunk.css"
+  },
+  {
+    "revision": "c9660f8f470727c1eb39",
+    "url": "/react-heifetz/static/js/2.6e1f2ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-heifetz/static/js/2.6e1f2ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831ae9014477a7b4a48c",
+    "url": "/react-heifetz/static/js/main.07f894f3.chunk.js"
+  },
+  {
+    "revision": "76ca71d0a0c3273f40f0",
+    "url": "/react-heifetz/static/js/runtime-main.664fa7e3.js"
+  }
+]);
