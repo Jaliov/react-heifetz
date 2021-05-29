d2b9b313f948cf89f215a8be5a884256f968f794
Joel Rudin <jarud55@gmail.com>
2021-05-28 20:31:43 -0400

    build

---
+++ build/precache-manifest.46cc2dda77dd6cb6a15dc8be65393d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0031bb7874cd53601459d3a4e47b2a",
+    "url": "/react-heifetz/index.html"
+  },
+  {
+    "revision": "552f76253f9bacda458e",
+    "url": "/react-heifetz/static/css/main.0d555add.chunk.css"
+  },
+  {
+    "revision": "d18dd90470a5b01a5feb",
+    "url": "/react-heifetz/static/js/2.880421bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-heifetz/static/js/2.880421bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552f76253f9bacda458e",
+    "url": "/react-heifetz/static/js/main.1ffc215a.chunk.js"
+  },
+  {
+    "revision": "76ca71d0a0c3273f40f0",
+    "url": "/react-heifetz/static/js/runtime-main.664fa7e3.js"
+  }
+]);
