9f802e831f8c3245c1e1de8dd519d23d9800c490
Joel Rudin <jarud55@gmail.com>
2023-10-01 07:25:11 -0400

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 // import PhotoDsply from './Photo';
-import Bio from "./bio";
+import Bio from "./Bio";
 
 const App = () => {
   return (
     <div className="App">
-      <Bio name="Jascha Heifetz" />
+      <Bio />
     </div>
   );
 };
